31e21346f5512a125a89b4d8ba1a76e1aaac61a6
TarunThakur7974 <tarunsinghthakur6261@gmail.com>
2023-12-09 10:39:38 +0530

    update

--- src/features/Auth/authService.js
+++ src/features/Auth/authService.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 
 let Register = async (formdata) => {
-  let responce = await axios.post("http://localhost:500/user/signup", formdata);
+  let responce = await axios.post("https://render.com/docs/node-version/user/signup", formdata);
   return responce.data;
 };
 
 let Login = async (formdata) => {
-  let responce = await axios.post("http://localhost:500/user/login", formdata);
+  let responce = await axios.post("https://render.com/docs/node-version/login", formdata);
   return responce.data;
 };
 
 let getUserData = async (formdata) => {
   if (formdata) {
-    let responce = await axios.get("http://localhost:500/notes/" + formdata);
+    let responce = await axios.get("https://render.com/docs/node-version/notes/" + formdata);
     return responce.data;
   }
 };
 let createData = async (formdata) => {
   if (formdata) {
     const { userId, title, description } = formdata
-    let responce = await axios.post("http://localhost:500/notes/" + userId, { title, description });
+    let responce = await axios.post("https://render.com/docs/node-version/notes/" + userId, { title, description });
     return responce.data;
   }
 };
 let deleteData = async (formdata) => {
   if (formdata) {
-    let responce = await axios.delete("http://localhost:500/notes/" + formdata);
+    let responce = await axios.delete("https://render.com/docs/node-version/notes/" + formdata);
     return responce.data;
   }
 };
